f7217883fe06a0fcdb078c6ca8ee30711cf9a9bd
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-03-01 16:10:19 -0500

    schemas file current

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -5,29 +5,27 @@ CREATE DATABASE business_db;
 \c business_db; 
 
 CREATE TABLE departments (
-    dept_id SERIAL PRIMARY KEY,
-    dept_name (VARCHAR 100),
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (  
-    job_title VARCHAR (100),
-    role_id SERIAL PRIMARY KEY,
-    salary INTEGER, 
-    FOREIGN KEY (dept_name)
-    REFERENCES departments(dept_name)
-    ON DELETE SET NULL
-
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    salary DECIMAL(10, 2), 
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 ); 
 
 CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR (30),
-    last_name VARCHAR (30),
-    role_id INTEGER,
-    dept_id INTEGER, 
-    manager_id INTEGER
-    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL,
-    FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE SET NULL
-    FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
-)
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
 
